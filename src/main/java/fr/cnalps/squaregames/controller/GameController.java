beab4d5c5cfe44bc97450e61b09180483bd0e397
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-11-22 17:10:50 +0100

    feat: Ajout du CrossOrigin dans GameController

--- src/main/java/fr/cnalps/squaregames/controller/GameController.java
+++ src/main/java/fr/cnalps/squaregames/controller/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import fr.le_campus_numerique.square_games.engine.InvalidPositionException;
 
 @RestController
 @RequestMapping("/games")
+@CrossOrigin(origins = "*")
 public class GameController {
 
     @Autowired
